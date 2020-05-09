e3086087a50b6baa21116be6b599794cdd83ed52
Denys <maletsden@gmail.com>
2020-05-09 22:19:03 +0300

    fix: fixed comments specified in PR #2 (Online store part 1)

--- src/components/Header/MobileHeader/ModileMenu.js
+++ src/components/Header/MobileHeader/ModileMenu.js
@@ -9,15 +9,14 @@ import {
 } from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import {withStyles} from "@material-ui/core/styles";
 
+import DrawerContent from "./DrawerContent/DrawerContent";
+import {MainComponentsSizes} from '../../Constants';
 
 import './MobileMenu.scss';
-import DrawerContent from "./DrawerContent";
 
-import {MainComponentsSizes} from '../../Constants';
-import makeStyles from "@material-ui/core/styles/makeStyles";
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = (theme) => ({
   toolbar: {
     minHeight: MainComponentsSizes.mobileHeaderHeight.md,
     [`${theme.breakpoints.up('xs')} and (orientation: landscape)`]: {
@@ -27,59 +26,71 @@ const useStyles = makeStyles((theme) => ({
       minHeight: MainComponentsSizes.mobileHeaderHeight.md,
     },
   },
-}));
+});
 
-export default function MobileMenu() {
-  const [drawerState, setDrawerState] = React.useState(false);
+class MobileMenu extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const toggleDrawer = (open) => (event) => {
+    this.state = {
+      drawerState: false
+    }
+  }
+
+  toggleDrawer(event, drawerState) {
     if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
     }
 
-    setDrawerState(open);
-  };
+    this.setState({
+      drawerState
+    });
+  }
 
-  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+  isIOS() {
+    return process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+  }
 
-  const classes = useStyles();
-  return (
-    <Box>
+  render() {
+    return (
+      <Box>
 
-      <AppBar>
-        <Toolbar className={`${classes.toolbar} mobileToolBar`}>
-          <Box className="mobileMenu">
-            <Typography variant="h6">
-              Fresh Flowers
-            </Typography>
+        <AppBar>
+          <Toolbar className={`${this.props.classes.toolbar} mobileToolBar`}>
+            <Box className="mobileMenu">
+              <Typography variant="h6">
+                Fresh Flowers
+              </Typography>
 
-            <Box>
-              <IconButton edge="start" color="inherit" aria-label="menu">
-                <ShoppingCartOutlinedIcon/>
-              </IconButton>
-              <IconButton edge="start" color="inherit" aria-label="menu"
-                          onClick={toggleDrawer(true)}>
-                <MenuIcon/>
-              </IconButton>
+              <Box>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                  <ShoppingCartOutlinedIcon/>
+                </IconButton>
+                <IconButton edge="start" color="inherit" aria-label="menu"
+                            onClick={event => this.toggleDrawer(event, true)}>
+                  <MenuIcon/>
+                </IconButton>
+              </Box>
+
             </Box>
+          </Toolbar>
+        </AppBar>
 
-          </Box>
-        </Toolbar>
-      </AppBar>
+        <React.Fragment>
+          <SwipeableDrawer
+            anchor={'left'}
+            open={this.state.drawerState}
+            onClose={event => this.toggleDrawer(event, false)}
+            onOpen={event => this.toggleDrawer(event, true)}
+            disableBackdropTransition={!this.isIOS()} disableDiscovery={this.isIOS()}
+          >
+            <DrawerContent/>
+          </SwipeableDrawer>
+        </React.Fragment>
 
-      <React.Fragment>
-        <SwipeableDrawer
-          anchor={'left'}
-          open={drawerState}
-          onClose={toggleDrawer(false)}
-          onOpen={toggleDrawer(true)}
-          disableBackdropTransition={!iOS} disableDiscovery={iOS}
-        >
-          <DrawerContent/>
-        </SwipeableDrawer>
-      </React.Fragment>
+      </Box>
+    );
+  }
+}
 
-    </Box>
-  );
-
-};
+export default withStyles(useStyles)(MobileMenu);
